3016af2c9faa6db92e05b69e860dc8dccc5ee5f1
Hans Pagel <mail@hanspagel.com>
2025-12-02 11:57:33 +0100

    Update package.json to use nodemon for development and add new devDependencies; create example OpenAPI document and refactor server to read from it.

--- server/index.ts
+++ server/index.ts
@@ -1,32 +1,9 @@
 import { serve } from '@hono/node-server'
 import { createMockServer } from '@scalar/mock-server'
+import { readFileSync } from 'node:fs'
 
-// Your OpenAPI document
-const document = {
-  openapi: '3.1.1',
-  info: {
-    title: 'Hello World',
-    version: '1.0.0',
-  },
-  paths: {
-    '/foobar': {
-      get: {
-        responses: {
-          '200': {
-            description: 'OK',
-            content: {
-              'application/json': {
-                example: {
-                  foo: 'bar',
-                },
-              },
-            },
-          },
-        },
-      },
-    },
-  },
-}
+// OpenAPI document
+const document = readFileSync('documents/example.json', 'utf8')
 
 // Create the mocked routes
 const app = await createMockServer({
@@ -36,6 +13,7 @@ const app = await createMockServer({
     console.log(context.req.method, context.req.path)
   },
 })
+
 // Start the server
 serve(
   {
